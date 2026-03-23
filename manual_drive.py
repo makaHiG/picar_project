329859c92239146921d7ebe4ceac314a522e2728
makaHiG <264442295+makaHiG@users.noreply.github.com>
2026-03-23 10:12:27 +0100

    fix

--- manual_drive.py
+++ manual_drive.py
@@ -166,7 +166,7 @@ def Roam():
                 bw.speed = SPEED
                 
                 if(distR and distL):
-                    target_angle= distR-distL#(sorted(distR)[len(distR)//2] -sorted(distL)[len(distL)//2])/100
+                    target_angle= distance_R-distance_R #(sorted(distR)[len(distR)//2] -sorted(distL)[len(distL)//2])/100
                     print("distR-distL ", target_angle)
                     #steer =(trendL-trendR)/100 
                     steer = (target_angle-angle)*k #- (gyro_z)*i
