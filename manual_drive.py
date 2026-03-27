ffd488e86f9d06b6eba11359a6fa25ad9e0da85b
makaHiG <264442295+makaHiG@users.noreply.github.com>
2026-03-27 15:51:34 +0100

    Added aproximate speed

--- manual_drive.py
+++ manual_drive.py
@@ -71,6 +71,8 @@ SPEED = 100       # speed 0-100 default 50
 TURN_TIME = 1.6
 #wheels.speed = SPEED
 TURN_SPEED = 100 #default 30
+Travel_Speed = 44*3.14/15 #Speed from test,cm/s
+
 def UpDownTest():
     camera_servo.turn_right()
     TakePhoto()
