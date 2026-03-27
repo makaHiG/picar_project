70a5fd8542d0fdcfbf08aae25b9118ce62fe3798
makaHiG <264442295+makaHiG@users.noreply.github.com>
2026-03-27 12:34:18 +0100

    Speed on

--- manual_drive.py
+++ manual_drive.py
@@ -67,10 +67,10 @@ debug = {
 
 # Steering & speed parameters
 STEER_ANGLE = 30  # degrees left/right
-SPEED = 0        # speed 0-100 default 50
+SPEED = 100       # speed 0-100 default 50
 TURN_TIME = 1.6
 #wheels.speed = SPEED
-TURN_SPEED = 0 #default 30
+TURN_SPEED = 30 #default 30
 def UpDownTest():
     camera_servo.turn_right()
     TakePhoto()
