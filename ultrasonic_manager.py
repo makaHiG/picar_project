4ee0d77f4e607211be9a646a204d7c81f875a1fc
makaHiG <264442295+makaHiG@users.noreply.github.com>
2026-03-31 17:07:46 +0200

    ------

--- ultrasonic_manager.py
+++ ultrasonic_manager.py
@@ -58,10 +58,10 @@ class UltrasonicManager:
         while True:
             self.front_distance = self.front.get_distance()
             time.sleep(0.06)
-            ##self.HandleUltrasonicData(self.left.get_distance(),self.left_values)
+            self.HandleUltrasonicData(self.left.get_distance(),self.left_values)
             self.left_distance = (sorted(self.left_values)[len(self.left_values)//2]) if self.left_values else 0
             time.sleep(0.06)
-            #self.HandleUltrasonicData( self.right.get_distance(),self.right_values)
+            self.HandleUltrasonicData( self.right.get_distance(),self.right_values)
             self.right_distance = (sorted(self.right_values)[len(self.right_values)//2]) if self.right_values else 0
             self.que.put((self.left_distance,self.front_distance,self.right_distance))
             time.sleep(0.06)
