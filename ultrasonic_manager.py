f0f2b71b3e066e51e991df2839238c5e9bf19376
makaHiG <264442295+makaHiG@users.noreply.github.com>
2026-04-01 17:23:39 +0200

    Debugging pin states

--- ultrasonic_manager.py
+++ ultrasonic_manager.py
@@ -61,9 +61,11 @@ class UltrasonicManager:
             self.HandleUltrasonicData(self.left.get_distance(),self.left_values)
             self.left_distance = (sorted(self.left_values)[len(self.left_values)//2]) if self.left_values else 0
             time.sleep(0.06)
-            self.HandleUltrasonicData( self.right.get_distance(),self.right_values)
-            self.right_distance = (sorted(self.right_values)[len(self.right_values)//2]) if self.right_values else 0
-            self.que.put((self.left_distance,self.front_distance,self.right_distance))
+            print("CheckR ", self.right.echo.value())
+            print("CheckL ", self.left.echo.value() )
+            #self.HandleUltrasonicData( self.right.get_distance(),self.right_values)
+            #self.right_distance = (sorted(self.right_values)[len(self.right_values)//2]) if self.right_values else 0
+            #self.que.put((self.left_distance,self.front_distance,self.right_distance))
             time.sleep(0.06)
 
             
