a89a89f6d645b11ad01dc54e51e7b5e757336627
makaHiG <264442295+makaHiG@users.noreply.github.com>
2026-04-10 20:22:53 +0200

    fix

--- manual_drive.py
+++ manual_drive.py
@@ -97,7 +97,7 @@ STEER_ANGLE = 30  # degrees left/right
 SPEED = 100       # speed 0-100 default 50
 TURN_TIME = 1.6
 #wheels.speed = SPEED
-TURN_SPEED = 100 #default 30
+TURN_SPEED = 30 #default 30
 Travel_Speed = 44*3.14/15 #Speed from test,cm/s
 
 
