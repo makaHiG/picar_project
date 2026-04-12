2ae7b4306b9436ffbb0b6f4f35b2e04b5b6f4071
makaHiG <264442295+makaHiG@users.noreply.github.com>
2026-04-12 11:22:20 +0200

    Right is good test L

--- manual_drive.py
+++ manual_drive.py
@@ -551,7 +551,7 @@ prev_time=time.time()
 state.mode=Mode.MANUAL
 try:
     while True:
-        print(UA_R.distance())
+        print(UA_L.distance())
         #sock.sendto(b"Hello", ("255.255.255.255", 5005))
         # if(get_key_nonblocking()=="m"):
         #     state.mode = Mode.MANUAL
