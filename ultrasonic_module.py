81486c18e7234b0f491f0b0e34c4af81ecae8f0a
makaHiG <264442295+makaHiG@users.noreply.github.com>
2026-04-13 10:53:01 +0200

    Testing changuing pluse length

--- ultrasonic_module.py
+++ ultrasonic_module.py
@@ -214,7 +214,7 @@ class Ultrasonic_Avoidance(object):
         timeout=0.03
         
         self.trig.low()
-        time.sleep(0.001)
+        time.sleep(0.01)
         self.trig.high()
         time.sleep(0.000015)
         self.trig.low()
