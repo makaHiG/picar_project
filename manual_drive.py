6553e010a0c788595d060d7199f387ca7b5cec2b
makaHiG <264442295+makaHiG@users.noreply.github.com>
2026-04-13 09:35:37 +0200

    testing align only

--- manual_drive.py
+++ manual_drive.py
@@ -278,9 +278,11 @@ def SteerCenter(state:RobotState):
     else:
         k=0
         k2=1
-
+    k=0
+    k2=1
+    
     trend = sum(state.center_errors)/len(state.center_errors) if len(state.center_errors)>0 else 0 
-    print("trend",trend)
+    #print("trend",trend)
     veer(trend*k+align_error*k2)
     
     # if(state.right_distance>0 and state.left_distance>0):
