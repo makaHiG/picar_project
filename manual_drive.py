0b5a2205c805bc744b3457fa19dea8397bf86c4f
makaHiG <264442295+makaHiG@users.noreply.github.com>
2026-04-13 11:49:30 +0200

    fiiix

--- manual_drive.py
+++ manual_drive.py
@@ -46,8 +46,8 @@ print(picar.back_wheels.__file__)
 # Initialize ultrasonic sensors
 sensor_queue = Queue()
 UA_F = UA2(20)
-UA_L = UA4('D13', 'D10')
-UA_R = UA4('D14', 'D12')
+UA_L = UA4(16,12)
+UA_R = UA4(26, 19)
 US_Manager =UltrasonicManager(UA_F, UA_L, UA_R,sensor_queue)
 
 ## SocketSetup
