5e49b2527086849225f23a862f1722df789d0144
makaHiG <264442295+makaHiG@users.noreply.github.com>
2026-04-13 15:02:09 +0200

    double calling

--- ultrasonic_manager.py
+++ ultrasonic_manager.py
@@ -65,10 +65,10 @@ class UltrasonicManager:
             self.front_distance = self.front.get_distance()
             time.sleep(0.05)
             self.HandleUltrasonicData(self.left.distance(),self.left_values)
-            self.left_distance = self.left.distance()#(sorted(self.left_values)[len(self.left_values)//2]) if self.left_values else 0
+            self.left_distance = (sorted(self.left_values)[len(self.left_values)//2]) if self.left_values else 0
             time.sleep(0.05)
             self.HandleUltrasonicData( self.right.distance(),self.right_values)
-            self.right_distance = self.right.distance()#(sorted(self.right_values)[len(self.right_values)//2]) if self.right_values else 0
+            self.right_distance = (sorted(self.right_values)[len(self.right_values)//2]) if self.right_values else 0
             #print(self.right_distance," | ",self.left_distance )
             self.que.put((self.left_distance,self.front_distance,self.right_distance))
             time.sleep(0.05)
