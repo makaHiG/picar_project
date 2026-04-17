688991f68a48ffc7efe3c7160d7f45a60c35d4f9
makaHiG <264442295+makaHiG@users.noreply.github.com>
2026-04-17 19:33:51 +0200

    bliss

--- manual_drive.py
+++ manual_drive.py
@@ -263,10 +263,10 @@ def SteerCenter(state:RobotState):
     
     center_error =0
     p=1
-    intCoeff=1
-    d=10
+    intCoeff=.1
+    d=.1
     kp_align=0.3
-    derivative = 10000
+    derivative = 0
     leftNoise,leftalign = state.Sensors.get_leftWallAngle() or (None,None)
     rightNoise,rightalign = state.Sensors.get_rightWallAngle() or (None,None)
     if(leftalign is not None and rightalign is not None):
