b7a3749fa350052d69e05a8342d21e6365cd28a6
makaHiG <264442295+makaHiG@users.noreply.github.com>
2026-04-18 16:46:01 +0200

    fix

--- manual_drive.py
+++ manual_drive.py
@@ -245,7 +245,7 @@ def RealRun(state:RobotState):
     subprocess.run([
         "v4l2-ctl",
         "-d", "/dev/video0",
-        "--set-fmt-video=width=1920,height=1080,pixelformat=MJPG",
+        "--set-fmt-video=width=1920,height=1080,pixelformat=MJPEG",
         "-c", "exposure_auto=1"
     ], check=True)
 
