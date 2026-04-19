f65005b9426dd0d16a43fe6dc2067086da2e89a4
makaHiG <264442295+makaHiG@users.noreply.github.com>
2026-04-19 16:10:28 +0200

    testing again, muting exposure time, setting gain to default, also trying MJPG

--- manual_drive.py
+++ manual_drive.py
@@ -222,10 +222,10 @@ def RealRun(state:RobotState):
     state.spinn.lastPhotoSpot=(state.x,state.y)
     subprocess.run([
     "v4l2-ctl", "-d", "/dev/video0",
-    "--set-fmt-video=width=1920,height=1080,pixelformat=YUYV",
+    "--set-fmt-video=width=1920,height=1080,pixelformat=MJPG",
     "-c", "auto_exposure=3",
-    "-c", "exposure_time_absolute=120",
-    "-c", "gain=0"
+    #"-c", "exposure_time_absolute=120",
+    "-c", "gain=57343"
     ])
     
     # subprocess.run([
