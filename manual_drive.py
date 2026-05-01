d84bc0d85e6ce42e816c95580fe89cf0c1f2e84c
makaHiG <264442295+makaHiG@users.noreply.github.com>
2026-05-01 17:23:32 +0200

    -.-

--- manual_drive.py
+++ manual_drive.py
@@ -170,7 +170,7 @@ def MoveToPoint(state:RobotState, point=None):
     def moveToPoint(state:RobotState):
         nonlocal nextBehavior
         nonlocal destination
-        if(np.linalg.norm(destination - state.position) < 10):
+        if(np.linalg.norm(destination, state.position) < 10):
             return nextBehavior
         else:
             MoveTo(state, destination)
