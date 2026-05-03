sign wrong?

manual_drive.py
@@ -498,8 +498,8 @@ def SteerCenter(state:RobotState):
             if(0<state.front_distance<100):
                 delta = state.Sensors.front_points[0]-state.world.centerMean
                 side = np.sign(np.dot(delta, state.world.centerNormal))
-                offset += side * buffer_distance * state.world.centerNormal
-                return MoveToPoint(state,state.Sensors.front_points[-1]+offset*state.world.centerNormal)
+                offset = side * buffer_distance * state.world.centerNormal
+                return MoveToPoint(state,state.Sensors.front_points[-1]-offset*state.world.centerNormal)
             new_center_mean = (l_mean + r_mean) / (2) + offset
 
 