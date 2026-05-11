ff7ccbc078da4e9dad1c8b263af290d09ca294ab
makaHiG <264442295+makaHiG@users.noreply.github.com>
2026-05-11 14:18:21 +0200

    Trying with higher alpha, to compensate for higher speed

--- manual_drive.py
+++ manual_drive.py
@@ -449,7 +449,7 @@ def SteerCenter(state:RobotState):
     derivative = 0
     integral = 0
     buffer_distance = 30
-    alpha = 0.15 * dt  # 0 = very stable, 1 = very reactive
+    alpha = 0.4 * dt  # 0 = very stable, 1 = very reactive
     def furtherPoint():
         p = np.array([state.x, state.y])
         c0 = state.world.centerMean
